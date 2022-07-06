2ed4bc88c3600ea14c5f64f816ef6dab794d586d
carolina sosa <carososar@gmail.com>
2022-07-06 12:00:40 -0300

    funcionamiento del login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 
 router.get('/', function(req,res,next){
@@ -8,6 +9,31 @@ router.get('/', function(req,res,next){
 //admin/layout.hbs    });
 
 })//wiew/admin/login.hbs
+})
+
+router.post('/', async function(req,res,next){
+    try{
+console.log(req.body);
+var usuario = req.body.usuario; //flavia
+var password = req.body.password; //1234
+
+var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+if( data != undefined){
+    res.redirect('/admin/novedades')
+
+} else{
+    res.render('admin/login',{ //login.hbs
+        layout:'admin/layout',
+        error:true
+    })
+}
+
+
+}catch(error){
+        console.log(error)
+}
+
 })
 
 module.exports = router;
